c6fa6f8e1b307ccba0bebf7c1d4d402af97aec6d
y24519 <y24519@iwate-it.ac.jp>
2025-03-03 10:27:48 +0900

    3/3

--- main.js
+++ main.js
@@ -2,23 +2,18 @@ document.addEventListener("DOMContentLoaded", function() {
     const buttons = document.querySelectorAll(".all button");
     const btnspace1 = document.querySelector(".space1");
     const btnspace2 = document.querySelector(".space2");
-    const btnspace3 = document.querySelector(".space3");
 
     let score = 0; // スコアを初期化
     let timer;
     let countdown;
     let timeLeft = 30; // タイマーの秒数を30に設定
     let isGameActive = false;
-<<<<<<< HEAD
     let max = 0;
 
     // BGMの設定
     const bgm = new Audio('image/Neon_Nights.mp3'); // BGMのファイルパスを指定
     bgm.loop = true; // BGMをループ再生する
     bgm.volume = 0.2; // BGMの音量を設定（ここでは20%に設定）
-=======
-    let max=0; //スコアの最大を0に設定
->>>>>>> 3adf65f13ca2c8bfd430d713d36f03b7775bd7b3
 
     // ランダムにボタンを表示する関数
     function toggleRandomButtons() {
@@ -32,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function() {
         // ランダムに1〜4個のボタンを表示する
         const numButtonsToShow = Math.floor(Math.random() * 4) + 1; // 1〜4のランダムな数
         let shownButtons = [];
-        
+
         while (shownButtons.length < numButtonsToShow) {
             const randomIndex = Math.floor(Math.random() * buttons.length);
             const button = buttons[randomIndex];
@@ -50,26 +45,18 @@ document.addEventListener("DOMContentLoaded", function() {
         isGameActive = false;
         buttons.forEach(button => {
             if (button !== btnspace1 && button !== btnspace2) {
-                button.style.display = "none";  //ボタンを非表示
+                button.style.display = "none"; //ボタンを非表示
             }
         });
-<<<<<<< HEAD
+
         if (max < score) {
             max = score;
-=======
-        if(max<score){ //maxよりscoreが大きいときmaxを入れ替える
-            max=score;
->>>>>>> 3adf65f13ca2c8bfd430d713d36f03b7775bd7b3
         }
+
         alert("ゲーム終了！得点は " + score + " 点です。 これまでの最高得点は " + max + "点です。");
     }
 
-<<<<<<< Updated upstream
     // 30秒からのカウントダウンの表示
-=======
-    
-
->>>>>>> Stashed changes
     function updateTimer() {
         timeLeft--;
         document.querySelector('.time').textContent = timeLeft;
@@ -127,9 +114,5 @@ document.addEventListener("DOMContentLoaded", function() {
         startGame();
     });
     btnspace2.addEventListener('click', stopGame);
-    btnspace3.addEventListener('click', function() {
-        bgm.pause(); // BGMを一時停止
-        bgm.currentTime = 0; // BGMを最初から再生できるようにする
-    });
 });
 
